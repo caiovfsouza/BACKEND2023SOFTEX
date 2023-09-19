a7da7f5dd4b4a61387d62cbf8ebb6ebb26e3efa3
Caio Souza <139229688+caiovfsouza@users.noreply.github.com>
2023-09-19 16:04:02 -0300

    calculadoraMedia.js

--- calculadoraMedia.js
+++ calculadoraMedia.js
@@ -1,9 +1,10 @@
-var nota
+var nota = 0
 var media = 0
 var quant = parseInt(prompt(`Quantas notas serão cadastras? `));
 
-for (let i = 1 ; i <= quant ; i++) {
-    nota = parseFloat(prompt(`Informe a ${i}ª nota :`));
-    media += nota / quant
-}
+    for (let i = 1 ; i <= quant ; i++) {
+        nota = parseFloat(prompt(`Informe a ${i}ª nota: `));
+        media += nota / quant
+    }
+
 console.log(`A média do aluno é: ${media.toFixed(2)}`);
