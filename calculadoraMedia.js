764576c5f56c89efe2634e0d52586c24d897eb52
Caio Souza <139229688+caiovfsouza@users.noreply.github.com>
2023-09-19 21:44:54 -0300

    calculadoraMedia.js

--- calculadoraMedia.js
+++ calculadoraMedia.js
@@ -1,10 +1,46 @@
-var nota = 0
-var media = 0
-var quant = parseInt(prompt(`Quantas notas serão cadastras? `));
+var a, b, c = false;
+var nota = 0;
+var media = 0;
+var quant, escolha;
 
-    for (let i = 1 ; i <= quant ; i++) {
-        nota = parseFloat(prompt(`Informe a ${i}ª nota: `));
-        media += nota / quant
+do {
+  do {
+    quant = parseInt(prompt(`Quantas notas serão cadastradas? `));
+
+    if (!isNaN(quant) && quant !== 0) {
+      a = true;
+    } else {
+      a = false;
+
+      console.error(`Opção inválida... Tente novamente!`);
     }
+  } while (a == false);
 
-console.log(`A média do aluno é: ${media.toFixed(2)}`);
+  for (let i = 1; i <= quant; i++) {
+    do {
+      nota = parseFloat(prompt(`Informe a ${i}ª nota: `));
+
+      if (nota >= 0 && nota <= 10 && !isNaN(nota)) {
+        b = true;
+
+        media += nota / quant;
+      } else {
+        b = false;
+
+        console.error(`Opção inválida... Tente novamente!`);
+      }
+    } while (b == false);
+  }
+
+  console.log(`A média do aluno é: ${media.toFixed(2)}`);
+
+  do {
+    escolha = prompt(`Deseja continuar? Sim|Não -> `);
+
+    if (escolha === `Sim` || escolha === `sim` || escolha === `SIM`) {
+      c = false;
+    } else {
+      c = true;
+    }
+  } while (c == false);
+} while (c == false);
