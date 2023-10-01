ea35e99743638d4ea95c1bae2274af457dbab138
Caio Souza <139229688+caiovfsouza@users.noreply.github.com>
2023-10-01 00:04:23 -0300

    funcao.js

--- funcao.js
+++ funcao.js
@@ -1,6 +1,7 @@
 function inicio() {
   console.log(`\nBem-vindo(a) a Jeff's Bakery!`);
 }
+
 function quanti(quant) {
   const listaP = [];
   const pedP = {
@@ -9,32 +10,35 @@ function quanti(quant) {
     valor: function valores() {
       var val;
       if (pedP.sabor == `Calabresa` && pedP.tamanho == `Grande`) {
-        val = 35.00;
+        val = 35.0;
       } else if (pedP.sabor == `Azeitona` && pedP.tamanho == `Grande`) {
         val = 35.0;
       } else if (pedP.sabor == `Tomate Seco` && pedP.tamanho == `Grande`) {
-        val = 40.00;
+        val = 40.0;
       } else if (pedP.sabor == `Parmesão` && pedP.tamanho == `Grande`) {
-        val = 40.00;
+        val = 40.0;
       } else if (pedP.sabor == `Calabresa` && pedP.tamanho == `Piccolo`) {
-        val = 20.00;
+        val = 20.0;
       } else if (pedP.sabor == `Azeitona` && pedP.tamanho == `Piccolo`) {
-        val = 20.00;
+        val = 20.0;
       } else if (pedP.sabor == `Tomate Seco` && pedP.tamanho == `Piccolo`) {
-        val = 22.50;
+        val = 22.5;
       } else if (pedP.sabor == `Parmesão` && pedP.tamanho == `Piccolo`) {
-        val = 22.50;
+        val = 22.5;
       } else {
-        val = 0.00;
+        val = 0.0;
       }
       return val;
     },
   };
-  var a, b = false;
+  var a,
+    b = false;
   for (let i = 1; i <= quant; i++) {
     console.clear();
     do {
-      console.log(`Escolha o sabor do ${i}º pão:\n\n(1) Calabresa\n(2) Azeitona\n(3) Tomate Seco\n(4) Parmesão\n`);
+      console.log(
+        `Escolha o sabor do ${i}º pão:\n\n(1) Calabresa\n(2) Azeitona\n(3) Tomate Seco\n(4) Parmesão\n`
+      );
       pedP.sabor = parseInt(prompt(`Sabor escolhido: `));
       if (pedP.sabor == 1) {
         a = true;
@@ -54,7 +58,9 @@ function quanti(quant) {
       }
     } while (a == false);
     do {
-      console.log(`\nEscolha o tamanho do ${i}º pão:\n\n(1) Grande\n(2) Piccolo\n`);
+      console.log(
+        `\nEscolha o tamanho do ${i}º pão:\n\n(1) Grande\n(2) Piccolo\n`
+      );
       pedP.tamanho = parseInt(prompt(`Tamanho escolhido: `));
       if (pedP.tamanho == 1) {
         b = true;
@@ -77,15 +83,47 @@ function quanti(quant) {
   }
   return listaP;
 }
+
 function fim() {
   console.log(`\nObrigado pela preferência!\n`);
 }
+var c,
+  d = false;
 inicio();
-var nome = prompt(`Por gentileza, informe o seu nome: `);
-var quant = parseInt(prompt(`Quantos pães você gostaria de encomendar, ${nome}? `));
-var escolhaP = quanti(quant);
-console.log(`\nRESUMO DO PEDIDO DE ${nome.toUpperCase()}:\n\n`,escolhaP,`\n\nQuantidade de pães: `,quant);
-const data = escolhaP.map((item) => item.preco).reduce((anterior, atual) => anterior + atual, 0);
-const total = data.toLocaleString("pt-BR", {style: "currency",currency: "BRL",});
-console.log(`\nTotal:`, total);
+do {
+  var nome = prompt(`Por gentileza, informe o seu nome: `);
+  if (isNaN(nome)) {
+    c = true;
+  } else {
+    c = false;
+    console.error(`Opção inválida! Tente novamente.`);
+  }
+} while (c == false);
+do {
+  var quant = parseInt(
+    prompt(`Quantos pães você gostaria de encomendar, ${nome}? `)
+  );
+  if (!isNaN(quant)) {
+    d = true;
+    var escolhaP = quanti(quant);
+    console.log(
+      `\nRESUMO DO PEDIDO DE ${nome.toUpperCase()}:\n\n`,
+      escolhaP,
+      `\n\nQuantidade de pães: `,
+      quant
+    );
+    const data = escolhaP
+      .map((item) => item.preco)
+      .reduce((anterior, atual) => anterior + atual, 0);
+
+    const total = data.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+    console.log(`\nTotal:`, total);
+  } else {
+    d = false;
+    console.error(`Opção inválida! Tente novamente.`);
+  }
+} while (d == false);
 fim();
