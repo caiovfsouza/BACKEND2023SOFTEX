ceb6ceb5ee52106e0a81f66f706f46a33d4f5b79
Caio Souza <139229688+caiovfsouza@users.noreply.github.com>
2023-10-30 20:17:23 -0300

    Update pedido.ts

--- Padrões de Desenvolvimento de Software/Fábrica de Computadores/ts/produto/pedido.ts
+++ Padrões de Desenvolvimento de Software/Fábrica de Computadores/ts/produto/pedido.ts
@@ -6,7 +6,7 @@ const PEDIDO3 = ProdutoFactory.getProduto(`TABLET`, 512, 0, 0);
 const PEDIDO4 = ProdutoFactory.getProduto(`CELULAR`, 128, 0, 0)
 
 
-console.debug(PEDIDO1.toString());
-console.debug(PEDIDO2.toString());  
-console.debug(PEDIDO3.toString());
-console.debug(PEDIDO4.toString());
+console.log(PEDIDO1.toString());
+console.log(PEDIDO2.toString());  
+console.log(PEDIDO3.toString());
+console.log(PEDIDO4.toString());
